3eab5959d86c9f5e49ba5974a5f2a38473fad83f
sathisht1998 <sathishth1998@gmail.com>
2024-02-13 12:06:01 -0600

    succesfully created http for navbar

---
+++ browser-app.js
@@ -0,0 +1,43 @@
+"use strict";
+const http = require('http');
+const { readFileSync } = require('fs');
+const homepage = readFileSync('./index.html');
+const homestyles = readFileSync('./styles.css');
+const homelogo = readFileSync('./logo.svg');
+const homelogic = readFileSync('./browser-app.ts');
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === "/") {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write(homepage);
+        res.end();
+    }
+    else if (url === "/about") {
+        res.writeHead(200, { 'contenttype': 'text/html' });
+        res.write('berify introduction');
+        res.end();
+    }
+    else if (url === "/styles.css") {
+        res.writeHead(200, { 'content-type': 'text/css' });
+        res.write(homestyles);
+        res.end();
+    }
+    else if (url === "/logo.svg") {
+        res.writeHead(200, { 'content-type': 'image/svg+xml' });
+        res.write(homelogo);
+        res.end();
+    }
+    else if (url === "/broswer-app.ts") {
+        res.writeHead(200, { 'content-type': 'text/typescript' });
+        res.write(homelogo);
+        res.end();
+    }
+    else {
+        res.writeHead(400, { 'content-type': 'text/html' });
+        res.write('page not found');
+        res.end();
+    }
+});
+server.listen(9009, () => {
+    console.log('server is running on port ');
+});
